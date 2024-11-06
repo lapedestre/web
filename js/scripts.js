b91d37b16b494c0aabeef873f27413e06b22d41f
Manuel <mra.code@hotmail.com>
2024-11-06 13:57:43 +0100

    dialogo cookies

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,36 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieConsent = document.getElementById('cookieConsent');
+    const acceptCookies = document.getElementById('acceptCookies');
+
+    // Verifica si la cookie de consentimiento ya está establecida
+    if (!getCookie('cookieConsent')) {
+        cookieConsent.style.display = 'block';
+    }
+
+    acceptCookies.addEventListener('click', function () {
+        setCookie('cookieConsent', 'true', 365);
+        cookieConsent.style.display = 'none';
+    });
+
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+});
